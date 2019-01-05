dfd08243d9f3415148d3c0c8489635ac4799abca
silvia-ventoruzzo <silvia.ventoruzzo@gmail.com>
2019-01-05 15:19:45 +0100

    Calendar

--- 01_functions.R
+++ 01_functions.R
@@ -22,7 +22,8 @@ df_join_clean <- function(df1, df2) {
   df_joined <- full_join(df1, df2, by = c("id", "host_id", "room_type", "price",
                                           "minimum_nights",
                                           "number_of_reviews", 
-                                          "latitude", "longitude")) %>% 
+                                          "latitude", "longitude",
+                                          "availability_365")) %>% 
     rename(lat = latitude,
            long = longitude) %>%
     # neighbourhood_group
@@ -105,48 +106,108 @@ distance_count <- function(main, reference, var_name, distance) {
 }
 
 ## Summary information for numeric and factor variables
+# summarize_df <- function(df, wrt = NULL, vars_mean, vars_count) {
+#   # Create empty vector
+#   summary_df <- c()
+#   # Variables of which we calculate the mean
+#   for (var in vars_mean) {
+#     var_name <- sym(var)
+#     if(is.null(wrt)) {
+#       mean_df <- df %>% summarize(!!var_name := mean(!!var_name))
+#       summary_df <- c(summary_df,mean_df)
+#     } else {
+#       wrt_name <- sym(wrt)
+#       mean_df <- df %>%
+#         group_by(!!wrt_name) %>% 
+#         summarize(!!var_name := mean(!!var_name))
+#       summary_df <- c(summary_df,mean_df)
+#       summary_df[duplicated(summary_df)] <- NULL
+#     }
+#   }
+#   summary_df <- as.data.frame(summary_df, stringsAsFactors = FALSE)
+#   # Variables of which we count the unique values of the factors
+#   for (var in vars_count) {
+#     var_name <- sym(var)
+#     if(is.null(wrt)) {
+#       count_df <- df %>%
+#         recode_all(var) %>%
+#         group_by(!!var_name) %>% 
+#         dplyr::summarize(count = n()) %>%
+#         spread(key = !!var_name, value = count)    
+#       summary_df <- summary_df %>% cbind(count_df)
+#     } else {
+#       wrt_name <- sym(wrt)
+#       count_df <- df %>%
+#         recode_all(var) %>%
+#         group_by(!!var_name, !!wrt_name) %>%
+#         dplyr::summarize(count = n()) %>%
+#         spread(key = !!var_name, value = count)  
+#       summary_df <- summary_df %>% full_join(count_df, by = wrt) %>%
+#         replace(is.na(.), 0)
+#     }
+#   }
+#   return(summary_df)
+# }
 summarize_df <- function(df, wrt = NULL, vars_mean, vars_count) {
   # Create empty vector
   summary_df <- c()
-  # Variables of which we calculate the mean
-  for (var in vars_mean) {
-    var_name <- sym(var)
-    if(is.null(wrt)) {
+  # For the case in which the summary is general
+  if(is.null(wrt)) {
+    # Variables of which we calculate the mean
+    for (var in vars_mean) {
+      var_name <- sym(var)
       mean_df <- df %>% summarize(!!var_name := mean(!!var_name))
       summary_df <- c(summary_df,mean_df)
-    } else {
-      wrt_name <- sym(wrt)
+    }
+    # Variables of which we count the unique values of the factors
+    for (var in vars_count) {
+      var_name <- sym(var)
+      count_df <- df %>%
+      recode_all(var) %>%
+      group_by(!!var_name) %>% 
+      dplyr::summarize(count = n()) %>%
+      spread(key = !!var_name, value = count)    
+      summary_df <- summary_df %>% cbind(count_df)
+    }
+  # For the case in which the summary is done wrt a variable
+  } else {
+    wrt_name <- sym(wrt)
+    # Variables of which we calculate the mean
+    for (var in vars_mean) {
+      var_name <- sym(var)
       mean_df <- df %>%
         group_by(!!wrt_name) %>% 
         summarize(!!var_name := mean(!!var_name))
       summary_df <- c(summary_df,mean_df)
       summary_df[duplicated(summary_df)] <- NULL
     }
-  }
-  summary_df <- as.data.frame(summary_df, stringsAsFactors = FALSE)
-  # Variables of which we count the unique values of the factors
-  for (var in vars_count) {
-    var_name <- sym(var)
-    if(is.null(wrt)) {
-      count_df <- df %>%
-        recode_all(var) %>%
-        group_by(!!var_name) %>% 
-        dplyr::summarize(count = n()) %>%
-        spread(key = !!var_name, value = count)    
-      summary_df <- summary_df %>% cbind(count_df)
-    } else {
-      wrt_name <- sym(wrt)
+    # Variables of which we count the unique values of the factors
+    for (var in vars_count) {
+      var_name <- sym(var)
       count_df <- df %>%
         recode_all(var) %>%
         group_by(!!var_name, !!wrt_name) %>%
         dplyr::summarize(count = n()) %>%
         spread(key = !!var_name, value = count)  
-      summary_df <- summary_df %>% full_join(count_df, by = wrt) %>%
+      summary_df <- summary_df %>% as.data.frame() %>%
+        dplyr::mutate(!!wrt_name := as.character(!!wrt_name)) %>%
+        full_join(count_df, by = wrt) %>%
         replace(is.na(.), 0)
     }
+    # Percentage of the rows
+    percentage_df <- df %>%
+      group_by(!!wrt_name) %>%
+      summarize(percentage = round(n()/nrow(.)*100, 2))
+    summary_df <- summary_df %>% full_join(percentage_df, by = wrt)
   }
   return(summary_df)
 }
+  
+
+  
+  
+
+
 
 ## Recode variable: rename all factor levels of that variable
 recode_all <- function(df, variable) {
